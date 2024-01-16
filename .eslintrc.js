242a62fc85895b1ec1ac5050c6bddd69969d118d
DanCoDesign <dancodesign@proton.me>
2024-01-16 21:55:38 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,8 @@ module.exports = {
   ignorePatterns: ['**/payload-types.ts'],
   plugins: ['prettier'],
   rules: {
-    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    'prettier/prettier': 0,
     'no-console': 'off',
   },
+  'prettier/prettier': 0,
 }
